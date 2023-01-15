8541e9f14ed93d3c15949326ab3fe827a97cdc51
Assetjan <asetziyabek@gmail.com>
2023-01-16 01:16:14 +0600

    solved

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int abc;
+        int sum = 0;
+        while (number != 0) {
+             abc = number%10;
+             sum += abc;
+             number /= 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int abc;
+        while (number != 0 ) {
+            abc = number%10;
+            System.out.print(abc);
+            number /= 10;
+        }
     }
 }
